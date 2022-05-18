2f46d500dd5fcfee8ef9f6c3999d89d6779cddc3
David Teixeira <david.teixeira.1680@minerva.pt>
2022-05-18 15:37:19 +0100

    Uchiha-Itachi

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsPage implements OnInit {
 
+public news = [
+  {title:"Isto é o titulo da primeira noticia", description:"Descricao da noticia", year:"2022", image:"link", icon:""},
+  {title:"Isto é o titulo da primeira noticia", description:"Descricao da noticia", year:"2022", image:"link", icon:""},
+  {title:"Isto é o titulo da primeira noticia", description:"Descricao da noticia", year:"2022", image:"link", icon:""},
+
+
+];
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
